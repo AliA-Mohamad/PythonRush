45d8c86aed27f823a725beccd00a021816aa9057
AliA-Mohamad <ali.mohamad.uc@gmail.com>
2026-07-20 11:20:42 -0300

    testando tipos

---
+++ extremamente básico/SetandoVar.py
@@ -0,0 +1,27 @@
+string = "texto"
+print("string:", string, "\ntipo:", type(string))
+
+numero = 10
+print("numero:", numero, "\ntipo:", type(numero))
+
+decimal = 10.5
+print("decimal:", decimal, "\ntipo:", type(decimal))
+
+vazio = None
+print("vazio:", vazio, "\ntipo:", type(vazio))
+
+lista = [1, 2, 3, 4, 5, 6]
+print("lista:", lista, "\nlista:", type(lista))
+
+dicionario = {"nome": "Ali", "idade": 24,}
+print("diconario:", dicionario, "\ntipo:", type(dicionario))
+
+tupla = (-23.605478807760733, -46.65219944627956)
+print("tupla:", tupla, "\ntipo", type(tupla))
+
+sequencia = range(0, 10)
+print("sequencia:", sequencia, "\ntipo:", type(sequencia))
+
+conjunto = {1, 2, 3, 3, 3, 4}
+print("conjunto:", conjunto, "\ntipo:", type(conjunto))
+
