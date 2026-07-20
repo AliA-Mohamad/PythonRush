ccd9ce4bd031329d26b517aeaefc6d6cae5e93d1
AliA-Mohamad <ali.mohamad.uc@gmail.com>
2026-07-20 11:34:50 -0300

    test conversion os types

---
+++ extremamente básico/TypeConversion.py
@@ -0,0 +1,12 @@
+stringNumber = '321'
+
+int(stringNumber)
+print("string:", stringNumber, "\ntypo:", type(stringNumber))
+print("string:", stringNumber, "\ntypo:", type(int(stringNumber)))
+
+print("\n")
+
+intNumber = 123
+floatNumber = 12.3
+newNumber = intNumber + floatNumber
+print("number:", newNumber, "\ntype:", type(newNumber))
