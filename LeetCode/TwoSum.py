e34ce2b7fb1270e699209fd578b53e1c41bf8f2d
AliA-Mohamad <ali.mohamad.uc@gmail.com>
2026-07-22 17:11:26 -0300

    LeetCode001

--- LeetCode/TwoSum.py
+++ LeetCode/TwoSum.py
@@ -1,7 +1,15 @@
-def twoSum(self, nums, target):
+def tentativa01(self, nums, target):
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
             if (i != j and nums[i] + nums[j] == target):
                 return [i, j]
     return []
 
+def tentativa02(self, nums, target):
+    map = {}
+    for i, num in enumerate(nums):
+        diferenca = target - num
+        if diferenca in map:
+            return [i, map[diferenca]]
+        map[num] = i
+    return []
