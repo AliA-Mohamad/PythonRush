963fa770ee9d5bebeaf01875c39bcbfe7cb96353
AliA-Mohamad <ali.mohamad.uc@gmail.com>
2026-07-27 19:58:06 -0300

    LeetCode003

--- LeetCode/LongestSubstringWithoutRepeatingCharacters.py
+++ LeetCode/LongestSubstringWithoutRepeatingCharacters.py
@@ -17,9 +17,18 @@ def primeira(s):
         return contagem
 
 def lengthOfLongestSubstring(s):
-    memo = []
-    
-    return 0
-   
+    pE = 0
+    r = 0
+    m = {}
+
+    for pD in range(len(s)):
+        if s[pD] not in m:
+            m[s[pD]] = pD
+        else:
+            pE = min(max(pE, m[s[pD]] + 1),pD) 
+            m[s[pD]] = pD
+        r = max(r, pD - pE + 1)
+
+    return r
                   
-print(lengthOfLongestSubstring(" ")) # 0
+print(lengthOfLongestSubstring("abba")) # 0
